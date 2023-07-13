04fa3f00bda4660c9e100ab1d496cf6172cb2354
SieJH <sjh.jason@gmail.com>
2023-07-13 18:07:47 +0800

    加入專案檔案。

---
+++ Msit147Site/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Msit147Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            // return Content("Hello World!!");
+            // return Content("<h2>Hello World!!</h2>","text/html");
+            //return Content("<h2>Ajax 您好!!</h2>", "text/html", System.Text.Encoding.UTF8);
+
+            return Content("Hello World!!", "application/msword");
+        }
+
+
+    }
+}
---
+++ Msit147Site/Controllers/Hw0713Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Msit147Site.Controllers
+{
+    public class Hw0713Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
