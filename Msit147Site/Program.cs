aa58e4aae63acdfeff981831ceebde8b6f90b03d
SieJH <sjh.jason@gmail.com>
2023-07-14 01:25:35 +0800

    Hw01 Jquery改寫

--- Msit147Site/Program.cs
+++ Msit147Site/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=HW0713}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
