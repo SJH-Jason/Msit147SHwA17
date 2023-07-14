983499a68cbe6d32c60557aa5c783aa180a822cc
SieJH <sjh.jason@gmail.com>
2023-07-14 16:32:43 +0800

    0714 上課資料

--- Msit147Site/Controllers/HomeController.cs
+++ Msit147Site/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace Msit147Site.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DemoContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,DemoContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
--- Msit147Site/Program.cs
+++ Msit147Site/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Msit147Site.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DemoContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DemoConnection")));
 
 var app = builder.Build();
 
